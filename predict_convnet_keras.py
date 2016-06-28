8896b8105bbc789585150ef7f6200eb06326fd61
guijo0 <cristovaopombo@gmail.com>
2016-06-28 10:06:53 +0100

    Added dice coefficient that can be passed as a custom loss function when training the model or doing predictions

--- predict_convnet_keras.py
+++ predict_convnet_keras.py
@@ -11,6 +11,8 @@ IMG_COLS = 32
 model = model_from_json(open('./data/convnet_keras.json').read())
 model.load_weights('./data/convnet_keras.h5')
 sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+
+# TODO: try using the dice coef here, to get predicitions that are closer to the actual leaderboard
 model.compile(optimizer=sgd, loss='categorical_crossentropy')
 
 test_data, test_id = load_data.load_test(IMG_ROWS, IMG_COLS)
@@ -27,6 +29,35 @@ def merge_several_folds_mean(data, nfolds):
         a += np.array(data[i])
     a /= nfolds
     return a.tolist()
+
+
+def dice(im1, im2):
+    """
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+    return 2. * intersection.sum() / (im1.sum() + im2.sum())
+
 
 test_res = merge_several_folds_mean(yfull_test, N_FOLDS)
 np.savez('./output/submission_data', test_res=test_res, test_id=test_id)
