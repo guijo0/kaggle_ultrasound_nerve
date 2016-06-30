7afc69eaef7975ad1dce6449cfed9b9591de792f
Sean Löfgren <lofgren.sean@gmail.com>
2016-06-30 12:47:33 +0100

    get rid of settings in json

--- settings.py
+++ settings.py
@@ -1,14 +1,7 @@
-import os
-import json
 
-
-with open('./SETTINGS.json') as settings:
-    SETTINGS = json.load(settings)
-
-TRAIN_DATA_PATH = SETTINGS["TRAIN_DATA_PATH"]
-TEST_DATA_PATH = SETTINGS["TEST_DATA_PATH"]
-MODEL_PATH = SETTINGS["MODEL_PATH"]
-SUBMISSION_PATH = SETTINGS["SUBMISSION_PATH"]
+TRAIN_DATA_PATH = "./data/train"
+TEST_DATA_PATH = "./data/test"
+SUBMISSION_PATH = "./output"
 
 IMG_ROWS = 32
 IMG_COLS = 32
